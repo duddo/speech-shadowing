9961a4a540edb5be009043d65ecb203e4417e3ce
duddo <dvd100@gmail.com>
2025-12-28 15:53:00 +0100

    Start Vue project

---
+++ frontend/src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+  import SpeechItem from "@/components/SpeechItem.vue";
+</script>
+
+<template>
+
+<div class="app">
+
+  <h1>Speech Shadowing</h1>
+
+  <SpeechItem />
+
+</div>
+
+</template>
+
+<style scoped>
+
+.app {
+    display: grid;
+    grid-template-rows: auto auto
+}
+
+</style>
