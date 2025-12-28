f7570a127885dbc1ad3b9b7eb0353cb5ea56d798
duddo <dvd100@gmail.com>
2025-12-28 15:53:20 +0100

    Exploring components

---
+++ frontend/src/components/SpeechItem.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+let showStart = ref(true);
+let showStop = ref(false);
+
+function startSpeaking() {
+  showStop.value = true;
+  showStart.value = false;
+}
+
+function stopSpeaking() {
+  showStart.value = true;
+  showStop.value= false;
+}
+</script>
+
+<template>
+  <div>
+    <button @click="startSpeaking"
+            :hidden="!showStart">
+            Start speaking</button>
+    <button @click="stopSpeaking"
+            :hidden="!showStop" >
+            Stop</button>
+    <audio id="audio"
+           hidden="hidden" controls />
+    <p id="result"
+       hidden="hidden" />
+    <button id="retry"
+            hidden="hidden" @click="startSpeaking" >
+            Retry</button>
+  </div>
+</template>
+
+<style scoped>
+div {
+  border: 1px solid red;
+  display: grid;
+  grid-template-columns: auto;
+  padding: 5px;
+}
+
+button {
+  margin: 5px;
+}
+
+audio {
+  margin: 5px;
+}
+</style>
